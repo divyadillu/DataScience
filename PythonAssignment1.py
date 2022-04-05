4104fd342b4528c268da66e584a647795673023e
Divya Narayana <36370924+divyadillu@users.noreply.github.com>
2022-04-05 23:44:30 +0530

    Add files via upload

---
+++ PythonAssignment1.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Get Current working directory
+
+# In[75]:
+
+
+import os
+os.getcwd()
+
+
+# In[76]:
+
+
+os.chdir('C:\\Users\\Divya Narayana\\1NCEPTEZ_TRAINING')
+
+
+# ## 2
+
+# In[77]:
+
+
+a= 10
+b='String'
+c= 10.20
+print(a, b, c)
+
+
+# In[78]:
+
+
+del(a,b,c)
+
+
+# # 3
+
+# In[79]:
+
+
+def= 'Divya'
+
+
+# # 4
+
+# In[80]:
+
+
+a= 1+2+3+
+   4-5+6+
+   7+8-10
+
+
+# In[81]:
+
+
+a= (10+2+3+
+   4-5+6+
+   7+8-10)
+a
+
+
+# # 5
+
+# In[82]:
+
+
+list(range(100,999,8))
+
+
+# # 6
+
+# In[83]:
+
+
+for a in range(10):
+    print(a)
+    if a/2:
+        break
+   
+
+
+# # 7
+
+# In[84]:
+
+
+def function1():
+    """Welcome"""
+    print('Love you India')
+
+
+# In[85]:
+
+
+function1()
+
+
+# In[86]:
+
+
+print(function1.__doc__)
+
+
+# # 8
+
+# In[87]:
+
+
+[a for a in range(100) if a%2 != 0]
+
+
+# In[88]:
+
+
+b='Divya Narayana'
+[a for a in b if a not in "aeiou"]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
